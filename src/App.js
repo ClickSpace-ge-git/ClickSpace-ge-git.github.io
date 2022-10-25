567698240d90ca092bbc0e459cc0b08bc5260478
ALXSAI <alexandersainishvili@gmail.com>
2022-10-25 21:02:14 +0400

    created routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import './App.scss';
 import AboutUsPage from "./pages/AboutUsPage/AboutUsPage";
 import HeaderStyle from "./components/HeaderStyle/HeaderStyle"
 
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import FooterStyle from "./components/FooterStyle/FooterStyle";
+import HomePage from "./pages/HomePage/HomePage";
+
 export default function App() {
   return (
-     <>
-        <HeaderStyle />
-        <AboutUsPage />
-
-     </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path ="/aboutus" element={
+                    <>
+                        <HeaderStyle/>
+                        <AboutUsPage/>
+                        <FooterStyle/>
+                    </>
+                }/>
+            </Routes>
+        </BrowserRouter>
   );
 }
